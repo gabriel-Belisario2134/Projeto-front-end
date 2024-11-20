e0b3fd22dd67a3f9eb45b14fb9bbdc138cb2eca7
J4d11 <jadesawandra@gmail.com>
2024-11-20 20:47:47 -0300

    As rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
 import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Arte from './components/arteEstetica/arte';
+import Home from './components/logoHome/Home';
+// import './logoHome/Home.css';
+import Header from './components/header/header';
 
 function App() {
   return (
-    <header className="header">
-      <h1 className="logo"><img src='/imagens/logo.png'/><a href='#'></a></h1>
-      <nav>
-        <ul className="nav">
-          <li><a href="#">Arte e Estetica</a></li>
-          <li><a href="#">Pontos turisticos</a></li>
-          <li><a href="#">Tradições e festivais</a></li>
-          <li><a href="#">Culinaria</a></li>
-          <li><a href="#">Cultura POP</a></li>
-          <li><a href="#">Login</a></li>
-        </ul>
-      </nav>
-    </header>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Header/>} />
+      <Route path="/Home" element={<Home />} />
+      <Route path="/arte" element={<Arte />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
+
 
 export default App
