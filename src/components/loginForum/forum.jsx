e9e7889d0314bdf488ac1b8a7a082279a29afdce
j4d11 <jadesawandra@gmail.com>
2024-11-22 09:39:13 -0300

    Imagem sakura

--- src/components/loginForum/forum.jsx
+++ src/components/loginForum/forum.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import './forum.css';
 
 const Forum = () => {
   return (
     <div>
       <h1>Bem-vindo ao Fórum!</h1>
-      <p>Aqui você pode interagir com outros usuários!</p>
+      <img src="/imagens/sakuraKinomoto.jpg" alt="Fórum" style={{ width: '100%', maxWidth: '600px', borderRadius: '8px' }} />
+      <h4>Aqui você pode interagir com outros usuários!</h4>
     </div>
   );
 };
